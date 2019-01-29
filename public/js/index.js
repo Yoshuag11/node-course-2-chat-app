c37e602d9681117bc0500a6cf3bc6df9fe9d0554
Hector Daniel Gameros <hectordgamerosm@gmail.com>
2019-01-29 15:54:10 -0800

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+const socket = io();
+
+socket.on( 'connect', () => {
+	console.log( 'Connected to the server' );
+	socket.emit( 'createMessage', {
+		from: 'hector@example.com',
+		text: 'Hey. This is Daniel'
+	} );
+} );
+socket.on( 'disconnect', () => console.log( 'Disconnected from server' ) );
+socket.on( 'newMessage', function ( message ) {
+	console.log( 'New message', message );
+} );
--- server/server.js
+++ server/server.js
@@ -13,6 +13,14 @@ app.use( express.static( publicPath ) );
 io.on( 'connection', socket => {
 	console.log( 'New user connected' );
 	socket.on( 'disconnect', () => console.log( 'User was disconnected' ) );
+	socket.emit( 'newMessage', {
+		from: 'daniel',
+		text: 'Hey. What is going on.',
+		createdAt: 123
+	} );
+	socket.on( 'createMessage', message => {
+		console.log( message );
+	} )
 } );
 server.listen( port, () => {
 	console.log( `Server is up on port ${ port }` );
