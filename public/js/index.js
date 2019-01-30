c8bf2256e06c90b1a48ad2ec1e0466812c1433f1
Hector Daniel Gameros <hectordgamerosm@gmail.com>
2019-01-30 15:58:31 -0800

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,20 @@
 const socket = io();
 
+function scrollToBottom () {
+	const messages = $( '#messages' );
+	const newMessage = messages.children( 'li:last-child' );
+	const clientHeight = messages.prop( 'clientHeight' );
+	const scrollTop = messages.prop( 'scrollTop' );
+	const scrollHeight = messages.prop( 'scrollHeight' );
+	const newMessageHeight = newMessage.innerHeight();
+	const lastMessageHeight = newMessage.prev().innerHeight();
+
+	if ( clientHeight + scrollTop + newMessageHeight + lastMessageHeight
+		  >= scrollHeight ) {
+		messages.scrollTop( scrollHeight );
+	}
+}
+
 socket.on( 'connect', () => {
 	console.log( 'Connected to the server' );
 } );
@@ -15,6 +30,8 @@ socket.on( 'newMessage', function ( message ) {
 		} );
 
 	$( '#messages' ).append( html );
+
+	scrollToBottom();
 } );
 socket.on( 'newLocationMessage', message => {
 	const formattedTime = moment( message.createdAt ).format( 'h:mm a' );
@@ -27,6 +44,8 @@ socket.on( 'newLocationMessage', message => {
 		} );
 
 	$( '#messages' ).append( html );
+
+	scrollToBottom();
 } );
 
 $( '#message-form' ).on( 'submit', function ( e ) {
